650eb6011d368e732d88523e213363da728303b3
Pavel Rorecek <pavel@rorecek.com>
2018-01-29 13:32:55 +0100

    Add HasUuid1 and HasUuid4 trait classes

---
+++ src/HasUuid4.php
@@ -0,0 +1,24 @@
+<?php
+namespace Vistag\EloquentUuid;
+
+use Ramsey\Uuid\Uuid;
+
+trait HasUuid4
+{
+    public static function bootHasUuid4()
+    {
+        static::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+		return false;
+    }
+}
